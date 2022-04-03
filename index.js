db7e77216f785239d914a00bdc875095bd534aa2
Joel Roed <jooel.roed@gmail.com>
2022-04-03 18:25:24 +0200

    Latest Update

--- index.js
+++ index.js
@@ -2,9 +2,30 @@
 
 
 async function getMovies() {
-    const response = await fetch('https://www.omdbapi.com/?apikey=746eca60&s=fast');
-    const data = await response.json();
-    
+    const response = await fetch("https://www.omdbapi.com/?apikey=746eca60&s=fast");
+    const moviesData = await response.json();
+    const moviesListEl = document.querySelector(".movie__wrapper--general");
+
+    moviesListEl.innerHTML = moviesData
+    .map(
+            (movie) => `<div class="movie__wrapper">
+    <h3 class="movie__title"><span class="purple__1">Movie Title</span></h3>
+    <img src="./img/fast6.jpg" alt="" class="movie__poster">
+    <h4 class="movie__desc">
+      <i class="fa-solid fa-calendar-days"></i>
+      <span class="purple__1">2004</span>
+    </h4>
+    <h4 class="movie__desc">
+      <i class="fa-solid fa-video"></i>
+      <span class="purple__1">Movie</span>
+    </h4>
+    <h4 class="movie__desc">
+      <i class="fa-brands fa-imdb"></i>
+      <span class="purple__1">Imdb tt5433138</span>
+    </h4>
+  </div>`
+  )
+  .join("");
 }
 
 getMovies();
